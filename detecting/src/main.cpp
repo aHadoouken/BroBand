08206a07548d5534a8e936b1a915518730ac0f24
Daniil Devyatkin <70203689+d3vyatk4ru@users.noreply.github.com>
2021-12-05 14:12:02 +0300

    Add files via upload

--- detecting/src/main.cpp
+++ detecting/src/main.cpp
@@ -4,7 +4,29 @@
 
 #include "detecting.h"
 
+#define RESNET32 "../../model/annotation_resnet34_porn_model.pt"
+
 int main() {
+
+    // создаем объект
+    PornImageDetector img_detector;
+
+    // загружаем модель
+    img_detector.load_model(RESNET32);
+
+    // читаем изображение
+    cv::Mat img = img_detector.load_img("../../test.jpg");
+
+    // предобработка
+    torch::Tensor img_tensor = img_detector.preproccesing(&img);
+
+    // прогоняем по сетке изображение
+    Probability probability = img_detector.forward(&img_tensor);
+
+    std::cout << probability;
+
+    // блюрим при необходимости
+    img_detector.blurring();
 
     return 0;
 }
