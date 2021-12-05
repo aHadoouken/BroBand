9250ab72a9c032fad9e010f1498335f016b9c221
Daniil Devyatkin <70203689+d3vyatk4ru@users.noreply.github.com>
2021-12-05 14:14:30 +0300

    Create trace_dlm2cpp.py

---
+++ detecting/model/trace_dlm2cpp.py
@@ -0,0 +1,31 @@
+import torch
+import os
+
+
+class ModuleForCpp(torch.nn.Module):
+
+    def __init__(self):
+
+        super(ModuleForCpp, self).__init__()
+
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            
+        self.resnet34 = torch.load(os.path.abspath('resnet34_porn_model.pth'),
+                                    map_location=device)
+        self.resnet34.eval()
+
+        self.resnet34 = torch.jit.trace(self.resnet34,
+                                      torch.rand((1, 3, 224, 224),
+                                      dtype=torch.float))
+
+    def forward(self, img):
+
+        return self.resnet34(img)
+        
+
+if __name__ == "__main__":
+
+    module = ModuleForCpp()
+    sm = torch.jit.script(module)
+
+    sm.save('annotation_resnet34_porn_model.pt')
