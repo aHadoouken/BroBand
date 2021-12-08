7ef06e37015adfe53c3e3a2ecd61fba6fbf4340f
d3vyatk4ru <danya.devyatkin@mail.ru>
2021-12-08 20:01:46 +0300

    Передаются ссылки

--- detecting/include/detecting.h
+++ detecting/include/detecting.h
@@ -16,6 +16,8 @@ struct Probability {
 
     Probability() : porn(0) {}
 
+    Probability(const double &prob) : porn(prob) {}
+
 public:
     friend std::ostream &operator<<(std::ostream &out, const Probability &prob);
 };
@@ -52,7 +54,7 @@ protected:
 public:
     int set_threshold(double _threshold);
 
-    virtual Probability forward(T *data) = 0;
+    virtual Probability forward(T &data) = 0;
 
     virtual int load_model(const std::string &path) = 0;
 };
@@ -63,25 +65,25 @@ private:
 
     cv::Mat base642mat(const std::string &base64_code);
 
-    std::string mat2base64(const cv::Mat *img);
+    std::string mat2base64(const cv::Mat &img);
 
 public:
     int load_model(const std::string &path) override;
 
     cv::Mat load_img(const std::string &path);
 
-    void permutation_channels(cv::Mat *img);
+    void permutation_channels(cv::Mat &img);
 
-    torch::Tensor preproccesing(cv::Mat *img);
+    torch::Tensor preproccesing(cv::Mat &img);
 
-    Probability forward(torch::Tensor *data) override;
+    Probability forward(torch::Tensor &data) override;
 
     std::string blurring();
 };
 
 class PornTextDetector : TorchWrapper<Message> {
 public:
-    Probability forward(Message *data) override;
+    Probability forward(Message &data) override;
 
     std::string preproccesing(std::string &text);
 
